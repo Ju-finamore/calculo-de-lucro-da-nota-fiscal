71b5c5a0417c338272ac69308ed8bee655dff93a
julia-finamore <juliamm_finamore@hotmail.com>
2023-01-20 12:52:17 -0300

    fiz toda a parte do back para calcular o lucro

--- app.js
+++ app.js
@@ -3,5 +3,12 @@ let income = prompt("Qual o valor da nota fiscal de entrada?");
 // Perguntar o valor da nota fiscal de saída
 let costs = prompt("Qual o valor da nota fiscal de saída?");
 // Perguntar o valor dos impostos deduzidos
-let taxPercent = prompt("Qual a porcentagemde dos impostos?");
-// Mostrar o lucro
+let taxPercent = prompt("Qual a porcentagem de dos impostos?");
+// Calcular o bruto
+let grossProfit = costs - income;
+// Valor dos impostos
+let tax = grossProfit * taxPercent / 100;
+// Valor do líquido
+let netIncome = grossProfit - tax;
+// Mostrar no site
+document.write(" Seu lucro é de $" + netIncome);
