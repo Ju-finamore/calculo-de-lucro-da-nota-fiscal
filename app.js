dd320d329924db5feb7c71cf0b7b6cab514ac9e9
julia-finamore <juliamm_finamore@hotmail.com>
2023-01-20 12:00:29 -0300

    criei 3 variáveis para receber as informações passadaspelo usuário

---
+++ app.js
@@ -0,0 +1,7 @@
+// Perguntas o valor da nofa fiscal de entrada
+let income = prompt("Qual o valor da nota fiscal de entrada?");
+// Perguntar o valor da nota fiscal de saída
+let costs = prompt("Qual o valor da nota fiscal de saída?");
+// Perguntar o valor dos impostos deduzidos
+let taxPercent = prompt("Qual a porcentagemde dos impostos?");
+// Mostrar o lucro
